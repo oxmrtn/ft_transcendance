8a5c4066970ebfaf6dc69f5f0b694dda3746aaa9
Eliott Bengtsson Maire <ebengtss@f2r3s4.clusters.42paris.fr>
2025-12-09 19:13:50 +0100

    Update

--- docker/front/entrypoint.sh
+++ docker/front/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "Starting container..."
 
-# Install dependencies if node_modules is missing or empty
 if [ ! -d "node_modules" ] || [ -z "$(ls -A node_modules 2>/dev/null)" ]; then
   echo "Installing dependencies..."
   npm install
