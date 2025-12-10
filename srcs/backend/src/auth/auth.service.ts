ce664e2d554ecec3ef6e5432b9b1f37f6e2f8fa4
Martin Trullard <mtrullar@f4r3s4.clusters.42paris.fr>
2025-12-10 18:33:10 +0100

    begin of auth, creation of several things

--- srcs/backend/src/auth/auth.service.ts
+++ srcs/backend/src/auth/auth.service.ts
@@ -1,7 +1,31 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 
+@Injectable
+export class AuthService 
+{
+    private readonly jwt_secret: string;
+    constructor(
+        private prisma: PrismaService,
+        private configService: ConfigService,
+    ){
+        this.jwt_secret = this.configService().get<string>('JWT_SECRET')
+        if (!this.jwt_secret) {
+            throw new Error('JWT_SECRET is not defined in the environment variables.');
+    }
+    }
 
-sss
+    async hashPassword(password : string) : Promise<string>
+    {
+        const salt = await bcrypt.genSalt();
+        return bcrypt.hash(password, salt);
+    }
+
+    async generateToken(userId : number) : Promise<string>
+    {
+        return jwt.sign({ userId }, this.jwt_secret, {expiresIn: '1h'});
+    }
+}
