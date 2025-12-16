dbe6e4c8a4e83e4b0fcbd5bcc7988a492d861417
Eliott Bengtsson Maire <ebengtss@f4r3s5.clusters.42paris.fr>
2025-12-16 17:18:24 +0100

    Started components

--- srcs/front/src/app/login/page.tsx
+++ srcs/front/src/app/login/page.tsx
@@ -1,7 +1,19 @@
+import Divider from '../../components/Divider';
+import Button from '../../components/Button';
+
 export default function Login() {
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      ...
+      <div className="relative flex bg-modal-bg border border-white/10 rounded-lg">
+        <div className="w-xs flex items-center justify-center">
+          img
+        </div>
+        <div className="flex flex-col items-center gap-4 py-12 px-6">
+          form
+          <Divider text="Se connecter" />
+          <Button text="Se connecter" primary={true} fullWidth={true} />
+        </div>
+      </div>
     </div>
   );
 }
