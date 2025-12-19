b0d7d4e82297fc014e30425d02fbc4caf71248f2
Eliott Bengtsson Maire <ebengtss@f4r3s3.clusters.42paris.fr>
2025-12-19 17:46:19 +0100

    Login page done

--- srcs/front/src/components/Button.tsx
+++ srcs/front/src/components/Button.tsx
@@ -2,24 +2,35 @@ import React from 'react';
 
 interface ButtonProps {
   fullWidth?: boolean;
-  primary?: boolean;
-  text: string;
+  disabled?: boolean;
+  type?: "button" | "submit";
+  style?: "primary" | "secondary";
+  children: React.ReactNode
   onClick?: () => void;
 }
 
-export default function Button({fullWidth, primary, text, onClick}: ButtonProps) {
-  if (!text)
-    throw new Error("Missing text prop");
+export default function Button({
+  fullWidth,
+  disabled,
+  type = "button",
+  style = "primary",
+  children,
+  onClick
+}: ButtonProps) {
+  if (!children)
+    throw new Error("Missing children prop");
 
-  const baseStyles = "py-2 px-4 rounded-md text-white font-bold border border-px border-white/10 transition-colors duration-200 hover:cursor-pointer";
+  const baseStyles = "py-2 px-4 rounded-md text-white font-bold border border-px border-white/10 transition-all duration-200 hover:cursor-pointer disabled:hover:cursor-default disabled:opacity-50";
   const widthStyles = fullWidth ? "w-full text-center" : "";
-  const colorStyles = primary
-    ? "bg-primary shadow-[0_0_20px] shadow-primary/70 hover:bg-primary/80"
+  const colorStyles = style === "primary"
+    ? "bg-primary shadow-[0_0_20px] shadow-primary/70 hover:bg-primary/80 disabled:hover:bg-primary"
     : "bg-white/10 hover:bg-white/20";
 
   const finalStyles = `${baseStyles} ${widthStyles} ${colorStyles}`;
 
   return (
-    <button onClick={onClick} className={finalStyles}>{text}</button>
+    <button disabled={disabled} type={type} onClick={onClick} className={finalStyles}>
+      {children}
+    </button>
   );
 }
