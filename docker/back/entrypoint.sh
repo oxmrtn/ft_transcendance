235ed810161e0313c22a97ffc99cddd936170ae1
jpiech <jpiech@student.42.fr>
2025-12-29 20:41:03 +0100

    modification du container backend pour que l initialisation se fasse au run et pas au build s il n a encore jamais ete fait pour pouvoir mapper le repertoire du container sur le repertoir de l host et supprimer les erreurs de syntaxes de VsCode + retrait du 's' de 'https' pour le fetch sur la page de login cote front pour que ca corresponde a l event du back pour lequel le SSL n est pas encore active

--- docker/back/entrypoint.sh
+++ docker/back/entrypoint.sh
@@ -6,9 +6,13 @@ echo "Starting container..."
 if [ ! -d "node_modules" ] || [ -z "$(ls -A node_modules 2>/dev/null)" ]; then
   echo "Installing dependencies..."
   npm install
+  echo "Generating prisma client..."
+  npx prisma generate
 else
   echo "Dependencies already installed"
 fi
 
+echo "Migrating database: $@"
+npx prisma migrate dev --name init
 echo "Starting application: $@"
 exec "$@"
