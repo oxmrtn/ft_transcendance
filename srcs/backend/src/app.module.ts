d4140d2954afbc41bfa8592507c0d54683a2f7e2
Quéren Somarriba <queren.somarriba@gmail.com>
2025-12-30 17:53:48 +0100

    ajout social module,toutes les fonctionnalitees a part le online status

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { SocialModule } from './social/social.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
       envFilePath: '.env',
     }),
     AuthModule,
+    SocialModule,
   ],
   controllers: [AppController],
   providers: [
---
+++ srcs/backend/src/social/social.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Delete, Query, Req, Param, Patch } from '@nestjs/common';
+import { SocialService } from './social.service';
+
+/* modifier req.user.id manuellement pout tester */
+@Controller('social')
+export class SocialController {
+	constructor(private readonly socialService: SocialService) {}
+
+	@Get('search')
+	search(@Query('q') query: string) {
+		return this.socialService.searchUsers(query);
+	}
+
+	@Get('friends')
+	getFriends(@Req() req) {
+		return this.socialService.getFriends(1);
+	}
+
+	@Post('request/:id')
+	sendRequest(@Req() req, @Param('id') receiverId: string) {
+		return this.socialService.sendFriendRequest(req.user.id, +receiverId);
+	}
+
+	@Patch('request/:id/accept')
+	accept(@Req() req, @Param('id') friendId: string) {
+		return this.socialService.handleRequest(req.user.id, +friendId, true);
+	}
+
+	@Patch('request/:id/reject')
+	reject(@Req() req, @Param('id') friendId: string) {
+		return this.socialService.handleRequest(req.user.id, +friendId, false);
+	}
+
+	@Delete('friends/:id')
+	remove(@Req() req, @Param('id') friendId: string) {
+		return this.socialService.removeFriend(req.user.id, +friendId);
+	}
+}
+
+
