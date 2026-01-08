649227ce8d6bfd2bd93db8fa4f0ea7df53388dc2
Eliott Bengtsson Maire <ebengtss@f4r3s5.clusters.42paris.fr>
2026-01-08 19:26:41 +0100

    Update

--- srcs/front/src/components/Header.tsx
+++ srcs/front/src/components/Header.tsx
@@ -1,61 +1,78 @@
 "use client";
 
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useAuth } from '../contexts/AuthContext';
 import { useModal } from '../contexts/ModalContext';
 import { useLanguage } from '../contexts/LanguageContext';
-import { Settings, LogOut } from 'lucide-react';
+import { Settings, LogOut, Menu, X } from 'lucide-react';
+import { cn } from '../lib/utils';
 import SettingsModal from './SettingsModal'
 import Nav from './Nav'
-Nav
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "./dropdown-menu"
+import { truncateSync } from 'node:fs';
 
 export default function Header() {
   const { logout, username, isAuthenticated } = useAuth();
   const { dictionary } = useLanguage();
   const { openModal } = useModal();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   if (!isAuthenticated || !dictionary)
     return null;
 
   return (
-    <div className="h-[80px] flex justify-between items-center gap-4 bg-modal-bg backdrop-blur-sm w-full border-b border-white/10 shadow-[0_0_30px] shadow-black/70 px-8">
-      <Link href="/">
+    <div className={cn(
+      "z-30 h-[64px] flex justify-between items-center gap-4 bg-modal-bg backdrop-blur-xl w-full px-8",
+      "md:border-b md:border-white/10 md:shadow-[0_0_30px] md:shadow-black/70",
+      !isMenuOpen ? "shadow-[0_0_30px] shadow-black/70" : ""
+    )}>
+      <Link href="/" className="md:z-auto z-50">
         <img className="h-6" src="/logo.png" />
       </Link>
-      <Nav tabs={[
-        { name: dictionary.header.friendsTab, href: "/friends" },
-        { name: dictionary.header.playTab, href: "/" },
-        { name: dictionary.header.historyTab, href: "/history" }
-      ]} />
-      <DropdownMenu>
-        <DropdownMenuTrigger className="text-sm font-mono rounded-full py-1.5 px-4 bg-white/5 border border-white/10 transition-colors duration-200 cursor-pointer hover:bg-white/10">
-          {username}
-        </DropdownMenuTrigger>
-        <DropdownMenuContent onCloseAutoFocus={(e) => e.preventDefault()} className="mr-2 bg-white/5 backdrop-blur-sm border border-white/10">
-          <DropdownMenuItem className="hover:bg-white/10" onClick={() => openModal(<SettingsModal />)}>
-            < Settings />
-            {dictionary.header.settings}
-          </DropdownMenuItem>
-          <DropdownMenuItem variant="destructive" onClick={logout}>
-            < LogOut />
-            {dictionary.header.logout}
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+
+      <div className={cn(
+        "z-20 absolute top-[64px] overflow-hidden left-0 w-full transition-[max-height] duration-400 border-b border-white/10 shadow-[0_20px_20px] shadow-black/30",
+        "md:relative md:top-0 md:w-fit md:h-full md:max-h-full md:transition-none md:border-0 md:shadow-none",
+        isMenuOpen ? "max-h-96" : "max-h-0",
+      )}>
+        <Nav tabs={[
+          { name: dictionary.header.friendsTab, href: "/friends" },
+          { name: dictionary.header.playTab, href: "/" },
+          { name: dictionary.header.historyTab, href: "/history" }
+        ]} />
+      </div>
+
+
+      <div className="flex items-center gap-4 md:z-auto z-50">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="text-sm font-mono rounded-full py-1.5 px-4 bg-white/5 border border-white/10 transition-colors duration-200 cursor-pointer hover:bg-white/10">
+            {username}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent onCloseAutoFocus={(e) => e.preventDefault()} className="mr-2 bg-white/5 backdrop-blur-sm border border-white/10">
+            <DropdownMenuItem className="hover:bg-white/10" onClick={() => openModal(<SettingsModal />)}>
+              < Settings className="mr-2 h-4 w-4" />
+              <span>{dictionary.header.settings}</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive" onClick={logout}>
+              < LogOut className="mr-2 h-4 w-4" />
+              <span>{dictionary.header.logout}</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <button
+          className="md:hidden p-2 text-white"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+      </div>
     </div>
   );
 }
--- srcs/front/src/components/Nav.tsx
+++ srcs/front/src/components/Nav.tsx
@@ -26,19 +26,25 @@ export default function Nav({ tabs }: NavProps) {
   if (!tabs || tabs.length === 0)
     throw new Error("Missing tabs prop");
 
+
   const cleanedPath = pathname.replace(`/${lang}`, '') || '/';
 
   return (
-    <div className="h-full flex">
+    <div className={cn(
+      "w-full flex flex-col items-center justify-center bg-modal-bg backdrop-blur-sm",
+      "md:h-full md:w-fit md:max-w-none md:translate-x-0 md:flex-row md:bg-transparent md:backdrop-blur-none"
+    )}>
       {tabs.map((tab, i) => (
         <Link
           key={i}
           href={tab.href} 
           className={cn(
-          "h-full flex items-center justify-center px-6 cursor-pointer transition-colors duration-200 border-b-2 uppercase font-semibold",
+          `w-full flex items-center justify-center py-4 px-6 cursor-pointer transition-colors duration-200 uppercase font-semibold
+          border-l-2
+          md:border-l-0 md:border-b-2 md:h-full`,
           cleanedPath === tab.href
-            ? "border-primary current-nav-gradient"
-            : "border-transparent text-white/50 hover:border-white/70 hover:text-white/70"
+            ? `border-primary bg-linear-to-r from-primary-30 to-transparent md:bg-linear-to-t`
+            : "border-transparent text-muted-text hover:border-white/70 hover:text-sub-text"
         )}>
           {tab.name}
         </Link>
