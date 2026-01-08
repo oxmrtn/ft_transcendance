17de238969fa7e19b8b1006f6d9053ece5648027
Eliott Bengtsson Maire <ebengtss@f4r3s5.clusters.42paris.fr>
2026-01-08 16:09:05 +0100

    Header done

--- srcs/front/src/components/Header.tsx
+++ srcs/front/src/components/Header.tsx
@@ -6,7 +6,8 @@ import { useModal } from '../contexts/ModalContext';
 import { useLanguage } from '../contexts/LanguageContext';
 import { Settings, LogOut } from 'lucide-react';
 import SettingsModal from './SettingsModal'
-
+import Nav from './Nav'
+Nav
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -31,22 +32,27 @@ export default function Header() {
     return null;
 
   return (
-    <div className="flex justify-between items-center gap-4 bg-modal-bg backdrop-blur-sm w-full border-b border-white/10 shadow-[0_0_30px] shadow-black/70 px-8 py-4">
+    <div className="h-[80px] flex justify-between items-center gap-4 bg-modal-bg backdrop-blur-sm w-full border-b border-white/10 shadow-[0_0_30px] shadow-black/70 px-8">
       <Link href="/">
         <img className="h-6" src="/logo.png" />
       </Link>
+      <Nav tabs={[
+        { name: dictionary.header.friendsTab, href: "/friends" },
+        { name: dictionary.header.playTab, href: "/" },
+        { name: dictionary.header.historyTab, href: "/history" }
+      ]} />
       <DropdownMenu>
-        <DropdownMenuTrigger className="text-sm font-mono rounded-full py-2 px-4 bg-white/5 border border-white/10 transition-colors duration-200 cursor-pointer hover:bg-white/10">
+        <DropdownMenuTrigger className="text-sm font-mono rounded-full py-1.5 px-4 bg-white/5 border border-white/10 transition-colors duration-200 cursor-pointer hover:bg-white/10">
           {username}
         </DropdownMenuTrigger>
         <DropdownMenuContent onCloseAutoFocus={(e) => e.preventDefault()} className="mr-2 bg-white/5 backdrop-blur-sm border border-white/10">
           <DropdownMenuItem className="hover:bg-white/10" onClick={() => openModal(<SettingsModal />)}>
             < Settings />
-            Settings
+            {dictionary.header.settings}
           </DropdownMenuItem>
           <DropdownMenuItem variant="destructive" onClick={logout}>
             < LogOut />
-            Log out
+            {dictionary.header.logout}
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
