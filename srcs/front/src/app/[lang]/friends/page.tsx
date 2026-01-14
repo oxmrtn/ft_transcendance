eaa1f3af26a66f54ef7df0fb88c5422dea4d1108
Eliott Bengtsson Maire <ebengtss@f4r1s3.clusters.42paris.fr>
2026-01-14 17:02:15 +0100

    Update

--- srcs/front/src/app/[lang]/friends/page.tsx
+++ srcs/front/src/app/[lang]/friends/page.tsx
@@ -1,32 +1,44 @@
 "use client";
 
+import { useState } from 'react';
 import React from 'react';
 import ContentWrapper from '../../../components/ContentWrapper';
 import AuthGuard from '../../../components/AuthGuard';
 import { useLanguage } from '../../../contexts/LanguageContext';
+import { useAuth } from '../../../contexts/AuthContext';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../components/tabs"
+import { TextInput } from '../../../components/Input';
+import Button from '../../../components/Button';
+
 
 export default function Page() {
+  const { isAuthenticated } = useAuth();
   const { dictionary } = useLanguage();
-  if (!dictionary)
+  const [isWaiting, setIsWaiting] = useState(true);
+
+  if (!isAuthenticated || !dictionary)
     return null;
 
   return (
     <AuthGuard>
-        <ContentWrapper title="friends">
-          <div className="flex items-center justify-center bg-black/20 border-b border-px border-white/10">
-            <Tabs defaultValue="friends">
-              <TabsList>
-                <TabsTrigger value="friends" className="tabs-trigger">friends</TabsTrigger>
-                <TabsTrigger value="pending" className="tabs-trigger">pending</TabsTrigger>
-              </TabsList>
-              <TabsContent value="friends">
-              </TabsContent>
-              <TabsContent value="pending">
-              </TabsContent>
-            </Tabs>
-          </div>
-        </ContentWrapper>
+      <ContentWrapper title="friends">
+        <Tabs defaultValue="friends">
+          <TabsList className="flex items-center justify-center bg-black/20 border-b border-px border-white/10">
+            <TabsTrigger value="friends" className="tabs-trigger">friends</TabsTrigger>
+            <TabsTrigger value="pending" className="tabs-trigger">pending</TabsTrigger>
+          </TabsList>
+          <TabsContent value="friends">
+            <div className="flex justify-between items-center fixed bottom-0 left-0 w-full p-4 border-t border-px border-white/10">
+              <div className="flex gap-2">
+                <TextInput customWidth="w-[174px]" placeholder="Search for a player" id="search-friend"/>
+                <Button variant="primary" onClick={() => {}}>add</Button>
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="pending">
+          </TabsContent>
+        </Tabs>
+      </ContentWrapper>
     </AuthGuard>
   );
 }
