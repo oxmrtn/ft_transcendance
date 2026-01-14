3894a93299c0967f9774afa1940a49d0aa238fb4
Eliott Bengtsson Maire <ebengtss@f4r1s3.clusters.42paris.fr>
2026-01-14 17:02:15 +0100

    Update

--- srcs/front/src/app/[lang]/history/page.tsx
+++ srcs/front/src/app/[lang]/history/page.tsx
@@ -3,11 +3,14 @@
 import React from 'react';
 import AuthGuard from '../../../components/AuthGuard';
 import ContentWrapper from '../../../components/ContentWrapper';
+import { useAuth } from '../../../contexts/AuthContext';
 import { useLanguage } from '../../../contexts/LanguageContext';
 
 export default function Page() {
+  const { isAuthenticated } = useAuth();
   const { dictionary } = useLanguage();
-  if (!dictionary)
+
+  if (!isAuthenticated || !dictionary)
     return null;
 
   return (
