76db27c549185573e41f1523ae00c2db79503d49
jpie94 <jordan.piech@gmail.com>
2026-01-19 17:52:33 +0100

    ajout d une strategie dans auth, je dois encore l integrer a des guards, et voir si j ai besoin d un validate custom par rapport a celui de Passport

---
+++ srcs/backend/src/auth/strategies/TokenStrat.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import {  ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class TokenStrat extends PassportStrategy(Strategy) {
+    constructor(private configservice : ConfigService){
+        super({
+            jwtFroRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configservice.get<string>('JWT_SECRET'), 
+        });
+    }
+}
