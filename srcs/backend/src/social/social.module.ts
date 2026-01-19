1f5eba55ba7eea3ca99c501587597363f9e9a4a7
jpiech <jpiech@student.42.fr>
2026-01-19 19:51:27 +0100

    rectification de la stratégie (il manquait une lettre dans l'extract jwt), ajout d'une fonction validate générique, et insertion de la stratégie dans un guard pour les controllers du social

--- srcs/backend/src/social/social.module.ts
+++ srcs/backend/src/social/social.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { SocialService } from './social.service';
 import { SocialController } from './social.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { TokenStrat } from 'src/auth/strategies/TokenStrat';
 
 @Module({
   imports: [PrismaModule],
-  providers: [SocialService],
+  providers: [
+	SocialService, 
+	TokenStrat
+],
   controllers: [SocialController],
 })
 export class SocialModule {}
