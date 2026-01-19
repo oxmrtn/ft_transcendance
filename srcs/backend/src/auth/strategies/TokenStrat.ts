50ea09a905f069e6358d427595dd0517a64ed140
jpiech <jpiech@student.42.fr>
2026-01-19 19:51:27 +0100

    rectification de la stratégie (il manquait une lettre dans l'extract jwt), ajout d'une fonction validate générique, et insertion de la stratégie dans un guard pour les controllers du social

--- srcs/backend/src/auth/strategies/TokenStrat.ts
+++ srcs/backend/src/auth/strategies/TokenStrat.ts
@@ -6,10 +6,14 @@ import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class TokenStrat extends PassportStrategy(Strategy) {
     constructor(private configservice : ConfigService){
-        super({
-            jwtFroRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
+        super({ //le mot clé "super" permet de faire appel au constructur de la classe parent pour l'initialiser correctement (sans ça Passport sait pas comment faire son taff)
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // là on précise où passport doit aller chercher le JWT : dans le header Authorization. Alors pourquoi bearer ? En fait y'a plusieurs types d'auth, et bearer c'est pour indiquer qu'il porte un token. Donc on doit avoir un header composé comme suit : Authorization : bearer TOKEN
+            ignoreExpiration: false, // ça ça pourrait se passer à true pour dev, et pas avoir à se relog pour avoir un nouveau token toutes les 15 min
             secretOrKey: configservice.get<string>('JWT_SECRET'), 
         });
     }
+
+validate (payload : any) {
+	return payload; //payload c'est ce qui se trouve à l'intérieur du JWT une fois décrypté grace à notre super secret qui se trouve dans l'env et qui est publiquement disponible sur github ! J'ai pas utilisé la fonction validate_token déjà présente dans auth, parce qu'il s'agit d'un appele à jwt.verify qui est en fait appelée sous le capot de validate par Passport 
+}
 }
