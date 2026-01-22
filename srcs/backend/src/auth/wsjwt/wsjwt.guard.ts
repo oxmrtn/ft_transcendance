02723c98763b82487055afcb1f12b6f01c4dfabd
queren-somarriba <queren.somarriba@gmail.com>
2026-01-22 19:21:38 +0100

    add authorization guard to chat gateway

---
+++ srcs/backend/src/auth/wsjwt/wsjwt.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Socket } from 'socket.io';
+import { WsException } from '@nestjs/websockets';
+
+
+@Injectable()
+export class WsJwtGuard implements CanActivate
+{
+	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>
+	{
+		if (context.getType() !== 'ws')
+			return true;
+
+		const client: Socket = context.switchToWs().getClient();
+		
+		if (!client.data.user)
+			throw new WsException('Unauthorized access');
+
+		return true;
+    }
+}
