aaa9f47a19869fc23d6db7e40c805daa9eb8585a
queren-somarriba <queren.somarriba@gmail.com>
2026-01-24 19:22:37 +0100

    game.gateway work for one unique game

--- srcs/backend/src/game/game.gateway.ts
+++ srcs/backend/src/game/game.gateway.ts
@@ -12,6 +12,7 @@ import { UseGuards } from "@nestjs/common";
 import { WsJwtGuard } from "src/auth/wsjwt/wsjwt.guard";
 import { codeSubmitDto } from "src/dto/code-submit.dto";
 import { createRoomDto } from "src/dto/create-room.dto";
+import { gameIdDto } from "src/dto/game-id.dto";
 
 @UseGuards(WsJwtGuard)
 @WebSocketGateway({cors: { origin: '*' } })
@@ -21,60 +22,107 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 	@WebSocketServer() server: Server;
 
 	private playerNb : number;
-	private readyPlayers = new Set<number>;
+	private gamePlayers = new Set<number>;
+	private roomedPlayer = new Set<number>;
 
-	handleConnection(@ConnectedSocket() client: Socket, @MessageBody() gameId: string) {}
+	handleConnection(@ConnectedSocket() client: Socket) {}
 
 	handleDisconnect(@ConnectedSocket() client: Socket)
 	{
 		//close la game, laisser les autres terminer, compter victoire pour adversaire si multiplayer????
 	};
 
-	@SubscribeMessage('createRoom')
+	@SubscribeMessage('create-room')
 	createGameRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: createRoomDto)
 	{
 		this.playerNb = payload.playerNb;
+		console.log('init playerNb: ', this.playerNb)
+		//verification gameId est unique
+		
+		this.roomedPlayer.add(client.data.user.userId);
 
 		client.join(`game_${payload.gameId}`);
-		this.server.to(`game_${payload.gameId}`).emit('gameInfo', {
-			newroom: `Welcome to Room ${payload.gameId}`,
-			player: `${client.data.user.username} has joined the battle !`
-		});//ca peut aussi etre seulement des IDs
+
+		this.server.to(`game_${payload.gameId}`).emit('game-info', {
+			event: 'create-room',
+			roomid: payload.gameId,
+			newplayer: client.data.user.username
+		});
 	}
 
-	@SubscribeMessage('leaveGame')
-	leaveGame(@ConnectedSocket() client: Socket, @MessageBody() gameId: string)
+	@SubscribeMessage('leave-game')
+	leaveGame(@ConnectedSocket() client: Socket, @MessageBody() payload: gameIdDto)
 	{
-		client.to(`game_${gameId}`).emit('gameInfo', {player: `${client.data.user.username} has leaved the battle !`});
-		this.readyPlayers.delete(client.data.user.userId);
-		if (this.readyPlayers.size === 1)
-			this.server.to(`game_${gameId}`).emit('gameInfo', "You won the Battle ! Congrats !");
+		client.to(`game_${payload.gameId}`).emit('game-info', {
+			event: 'game-leave',
+			player: client.data.user.username
+		});
+
+		this.gamePlayers.delete(client.data.user.userId);
+		
+		if (this.gamePlayers.size === 1)
+			this.server.to(`game_${payload.gameId}`).emit('game-info', {
+				event: 'results',
+				winner: this.gamePlayers[0]
+			});
 	}
 
-	@SubscribeMessage('joinGame')
-	handleJoinGame(@ConnectedSocket() client: Socket, @MessageBody() gameId: string)
+	@SubscribeMessage('leave-room')
+	leaveRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: gameIdDto)
 	{
-		client.join(`game_${gameId}`);
-		client.to(`game_${gameId}`).emit('gameInfo', {player: `${client.data.user.username} has joined the battle !`});
+		client.to(`game_${payload.gameId}`).emit('game-info', {
+			event: 'leave-room',
+			player: client.data.user.username
+		});
+
+		this.roomedPlayer.delete(client.data.user.userId);
 	}
 
-	@SubscribeMessage('codeSubmit')
+	@SubscribeMessage('join-room')
+	handleJoinGame(@ConnectedSocket() client: Socket, @MessageBody() payload: gameIdDto)
+	{
+		if (this.roomedPlayer.has(client.data.user.userId))
+				return;
+		
+		client.join(`game_${payload.gameId}`);
+
+		this.roomedPlayer.add(client.data.user.userId);//limite d'users present dans la room??
+
+		client.to(`game_${payload.gameId}`).emit('game-info', {
+			event: 'join-room',
+			player: client.data.user.username
+		});
+	}
+
+	@SubscribeMessage('code-submit')
 	async handleCodeSubmit(@ConnectedSocket() client: Socket, @MessageBody() payload: codeSubmitDto)
 	{
-		client.to(`game_${payload.gameId}`).emit('gameInfo', `${client.data.user.username} submit his code !`);
-		//passer le code a l'api de test
+		client.to(`game_${payload.gameId}`).emit('game-info', {
+			event: 'code-submit',
+			player: client.data.user.username
+		});
+		
+		//passer le code a l'api de tests
 	}
 
-	@SubscribeMessage('setReady')
-	launchBattle(@ConnectedSocket() client: Socket, @MessageBody() gameId: string)
+	@SubscribeMessage('join-game')
+	launchBattle(@ConnectedSocket() client: Socket, @MessageBody() payload: gameIdDto)
 	{
-		if (this.readyPlayers.has(client.data.user.userId))
+		if (this.gamePlayers.has(client.data.user.userId) || !this.roomedPlayer.has(client.data.user.userId))
 			return;
 
-		client.to(`game_${gameId}`).emit('gameInfo', `${client.data.user.username} is ready !`);
-		this.readyPlayers.add(client.data.user.userId);
+		client.to(`game_${payload.gameId}`).emit('game-info', {
+			event: 'join-game',
+			player: client.data.user.username
+		});
 
-		if (this.readyPlayers.size === this.playerNb)
-			this.server.to(`game_${gameId}`).emit('gameInfo', 'Start battle !');
+		this.gamePlayers.add(client.data.user.userId);
+
+		if (this.gamePlayers.size === this.playerNb)
+		{
+			this.server.to(`game_${payload.gameId}`).emit('game-info', {
+				event: 'start' 	
+			});
+		}
 	}
 }
