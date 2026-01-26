dbc47fac7a1a67ebeee41cceb063bbd152712106
queren-somarriba <queren.somarriba@gmail.com>
2026-01-26 19:21:20 +0100

    add clientToRoom for handleDisconnect

--- srcs/backend/src/game/game.gateway.ts
+++ srcs/backend/src/game/game.gateway.ts
@@ -34,45 +34,83 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 
 	@WebSocketServer() server: Server;
 
-	private gamesMap = new Map<string, GameSession>();
+	private gameSessions = new Map<string, GameSession>();
+	private clientToRoom = new Map<number, string>();
 
 
 	handleConnection(@ConnectedSocket() client: Socket) {}
 
 	handleDisconnect(@ConnectedSocket() client: Socket)
 	{
-		//close la game, laisser les autres terminer, compter victoire pour adversaire si multiplayer????
-	};
+		const userId : number = client.data.user.userId;
+		const gameId :string = this.clientToRoom.get(userId);
+		const currentGame = this.gameSessions.get(gameId);
+	
+		if (!gameId || !currentGame)
+			return;
+
+		if (this.clientToRoom.has(userId))
+			this.clientToRoom.delete(userId);
+
+		if (currentGame.gamePlayers.has(userId))
+			currentGame.gamePlayers.delete(userId);
+
+		if (currentGame.gamePlayers.size === 1)
+		{
+			this.server.to(`game_${gameId}`).emit('game-info', {
+				event: 'results',
+				winner: currentGame.gamePlayers[0]
+			});
+			currentGame.gamePlayers.clear;
+		}
+
+		if (currentGame.roomPlayers.has(userId))
+			currentGame.roomPlayers.delete(userId);
+
+		if (!currentGame.roomPlayers.size)
+			this.gameSessions.delete(gameId);
+
+		this.server.to(`game_${gameId}`).emit('game-info', {
+			event: 'room-status',
+			roomPlayers: currentGame.roomPlayers,
+			gamePlayers: currentGame.gamePlayers,
+			playerNumber: currentGame.playerNumber,
+			gameId: currentGame.gameId
+		});
+	}
 
 	@SubscribeMessage('create-room')
 	createGameRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: createRoomDto)
 	{
 		const gameId = payload.gameId;
 
-		if (this.gamesMap.has(gameId))
-			return client.emit('game-info', {
-		event: 'error',
-		message: 'This room already exist !'
-	});
+		if (this.gameSessions.has(gameId))
+		{
+			this.errorMessage(client, `Room ${gameId} already exist !`);
+			return;
+		}
 		
 		const playerNumber = payload.playerNumber;
-		const user = client.data.user;
+		const userId : number = client.data.user.userId;
 
-		this.gamesMap.set(gameId, new GameSession(gameId, playerNumber));
+		if (this.clientToRoom.has(userId))
+		{
+			this.errorMessage(client, `${client.data.user.username} already join anonther room!`);
+			return;
+		}
 
-		const currentGame = this.gamesMap.get(gameId);
+		this.gameSessions.set(gameId, new GameSession(gameId, playerNumber));
+
+		const currentGame = this.gameSessions.get(gameId);
 		
 		currentGame.roomPlayers = new Set();
-		currentGame.roomPlayers.add(user.userId);
+		currentGame.roomPlayers.add(userId);
 		currentGame.gamePlayers = new Set();
+		this.clientToRoom.set(userId, gameId);
 
 		client.join(`game_${gameId}`);
 
-		this.server.to(`game_${gameId}`).emit('game-info', {
-			event: 'create-room',
-			roomid: gameId,
-			newplayer: user.username
-		});
+		this.notifyGameStatus(client);
 	}
 
 	@SubscribeMessage('join-room')
@@ -80,7 +118,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 	{
 		const gameId = payload.gameId;
 		const user = client.data.user;
-		const currentGame = this.gamesMap.get(gameId);
+		const currentGame = this.gameSessions.get(gameId);
 
 		if (!currentGame)
 		{
@@ -93,6 +131,12 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 			this.errorMessage(client, `${user.username} already join this room!`);
 			return;
 		}
+
+		if (this.clientToRoom.has(user.userId))
+		{
+			this.errorMessage(client, `${user.username} already join another Room!`);
+			return;
+		}
 		
 		client.join(`game_${gameId}`);
 
@@ -103,11 +147,9 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 		}
 
 		currentGame.roomPlayers.add(user.userId);
+		this.clientToRoom.set(user.userId, gameId);
 
-		client.to(`game_${gameId}`).emit('game-info', {
-			event: 'join-room',
-			player: user.username
-		});
+		this.notifyGameStatus(client);
 	}
 
 	@SubscribeMessage('leave-room')
@@ -115,7 +157,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 	{
 		const gameId = payload.gameId;
 		const user = client.data.user;
-		const currentGame = this.gamesMap.get(gameId);
+		const currentGame = this.gameSessions.get(gameId);
 
 		if (!currentGame)
 		{
@@ -129,14 +171,11 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 			return;
 		}			
 
-		client.to(`game_${gameId}`).emit('game-info', {
-			event: 'leave-room',
-			player: user.username
-		});
-
 		currentGame.roomPlayers.delete(user.userId);
+		this.clientToRoom.delete(user.userId);
+
 		if (!currentGame.roomPlayers.size)
-			this.gamesMap.delete(gameId);
+			this.gameSessions.delete(gameId);
 	}
 
 	@SubscribeMessage('join-game')
@@ -144,7 +183,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 	{
 		const gameId = payload.gameId;
 		const user = client.data.user;
-		const currentGame = this.gamesMap.get(gameId);
+		const currentGame = this.gameSessions.get(gameId);
 
 		if (!currentGame)
 		{
@@ -170,12 +209,9 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 			return;
 		}
 
-		client.to(`game_${gameId}`).emit('game-info', {
-			event: 'join-game',
-			player: user.username
-		});
-
 		currentGame.gamePlayers.add(user.userId);
+
+		this.notifyGameStatus(client);
 
 		if (currentGame.gamePlayers.size === currentGame.playerNumber)
 		{
@@ -190,7 +226,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 	{
 		const gameId = payload.gameId;
 		const user = client.data.user;
-		const currentGame = this.gamesMap.get(gameId);
+		const currentGame = this.gameSessions.get(gameId);
 
 		if (!currentGame)
 		{
@@ -204,18 +240,16 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 			return;
 		}
 
-		client.to(`game_${gameId}`).emit('game-info', {
-			event: 'game-leave',
-			player: user.username
-		});
-
 		currentGame.gamePlayers.delete(user.userId);
 		
 		if (currentGame.gamePlayers.size === 1)
+		{
 			this.server.to(`game_${gameId}`).emit('game-info', {
 				event: 'results',
 				winner: currentGame.gamePlayers[0]
 			});
+			currentGame.gamePlayers.clear;
+		}
 	}
 
 	@SubscribeMessage('code-submit')
@@ -223,7 +257,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 	{
 		const gameId = payload.gameId;
 		const user = client.data.user;
-		const currentGame = this.gamesMap.get(gameId);
+		const currentGame = this.gameSessions.get(gameId);
 
 		if (!currentGame)
 		{
@@ -250,5 +284,19 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 				event: 'error',
 				message: msg
 			});
+	}
+
+	private notifyGameStatus(@ConnectedSocket() client: Socket)
+	{
+		const userId : number = client.data.user.userId;
+		const currentGame = this.gameSessions.get(this.clientToRoom.get(userId))
+
+		this.server.to(`game_${currentGame.gameId}`).emit('game-info', {
+			event: 'room-status',
+			roomPlayers: currentGame.roomPlayers,
+			gamePlayers: currentGame.gamePlayers,
+			playerNumber: currentGame.playerNumber,
+			gameId: currentGame.gameId
+		});
 	}
 }
