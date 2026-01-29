836c758cf18bb829b39c9c7fe0a86d7078565e4c
jpie94 <jordan.piech@gmail.com>
2026-01-29 17:48:20 +0100

    nettoyage du pipe trim ou j avais laisse des console.log, migration de la DB pour enlever le caractere unique du password, deplacement de toute la logique vers les services pour update, gestion d'erreurs, ajout de la route pour recuperer les informations d un profil

--- srcs/backend/src/profile/profile.module.ts
+++ srcs/backend/src/profile/profile.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { TokenStrat } from 'src/auth/strategies/TokenStrat';
+import { AuthService } from 'src/auth/auth.service';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
+  imports : [PrismaModule],
   controllers: [ProfileController],
-  providers: [ProfileService,
-    TokenStrat
+  providers: [
+    ProfileService,
+    TokenStrat,
+    AuthService
   ],
 })
 export class ProfileModule {}
