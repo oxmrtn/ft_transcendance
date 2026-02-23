5e0725e5e603205805a498fab85f06a6c514f94f
queren-somarriba <queren.somarriba@gmail.com>
2026-02-23 18:54:51 +0100

    add exercices subjects

--- srcs/sandbox/tester/ref_functions/strlen.c
+++ srcs/sandbox/tester/ref_functions/strlen.c
@@ -1,3 +1,20 @@
+/*
+Assignment name  : ft_strlen
+Expected files   : ft_strlen.c
+Allowed functions: none
+-------------------------------------------------------------------------------
+
+Write a function named ft_strlen that takes a string as a parameter and returns
+its length.
+
+The length of a string is the number of characters that precede the terminating
+NUL character.
+
+Your function must be declared as follows:
+
+int ft_strlen(char *str);
+*/
+
 int ft_strlen(char* str)
 {
 	char *start = str;
