b3eafc28703390737e3fb7006ab65d4b080121ea
queren-somarriba <queren.somarriba@gmail.com>
2026-02-26 19:22:44 +0100

    fix social issue with room id

--- srcs/backend/src/social/social.gateway.ts
+++ srcs/backend/src/social/social.gateway.ts
@@ -43,6 +43,7 @@ export class SocialGateway implements OnGatewayConnection, OnGatewayDisconnect
 		if (this.onlineUsers.get(userId).size === 1)
 			await this.handleOnlineUser(user);
 
+		console.log("connect: ", `user_${userId}`);
 		await client.join(`user_${userId}`);
 	}
 
@@ -141,12 +142,12 @@ export class SocialGateway implements OnGatewayConnection, OnGatewayDisconnect
 
 	public friendshipRemoved(user1: any, user2: any)
 	{
-		this.server.to(`user_${user2.userId}`).emit('user-status', {
+		this.server.to(`user_${user2.id}`).emit('user-status', {
 			username: user1.username,
 			status: false
 		});
 
-		this.server.to(`user_${user1.userId}`).emit('user-status', {
+		this.server.to(`user_${user1.id}`).emit('user-status', {
 			username: user2.username,
 			status: false
 		});
