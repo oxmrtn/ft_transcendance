0b59b35f49afa86af05a41889de829d7a3e702bc
jpiech <jpiech@student.42.fr>
2026-03-02 18:26:31 +0100

    ajout d'une limite a 5mb dans la config d nginx pour override la limite par defaut a 1mb, modification des messages du front affirmant que la limite etait a 10mb, ajout d'une verification des magic bytes des fichiers uploades pour s assurer qu'ils correspondent bien a leur mimetype

--- srcs/backend/src/profile/profile.service.ts
+++ srcs/backend/src/profile/profile.service.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs';
 import { PrismaService } from '../../prisma/prisma.service';
 import { AuthService } from 'src/auth/auth.service';
 import * as bcrypt from 'bcrypt'
+import { buffer } from 'stream/consumers';
 
 @Injectable()
 export class ProfileService {
@@ -42,9 +43,31 @@ export class ProfileService {
 	async updatePicture(picture: MultipartFile, userId: number) {
 		if (picture.mimetype !== "image/jpeg" && picture.mimetype !== "image/png")
 			throw new UnsupportedMediaTypeException('Profile picture must be jpg or png only.');
+		const buffer = await picture.toBuffer();
+		await this.checkMagicBytes(buffer, picture.mimetype);
 		const picPath = `/pictures/${userId}_avatar.jpg`;
-		fs.promises.writeFile(`.${picPath}`, await picture.toBuffer());
+		fs.promises.writeFile(`.${picPath}`, buffer);
 		return await this.prisma.user.update({ where: { id: userId }, data: { profilePictureUrl: picPath } });
+	}
+
+	async checkMagicBytes(Buffer : Buffer<ArrayBufferLike>, mimetype : string)
+	{
+		if (mimetype === "image/png" && Buffer.length >= 8 && 
+			Buffer[0]=== 0x89 &&
+			Buffer[1]=== 0x50 && 
+			Buffer[2]=== 0x4E &&
+			Buffer[3]=== 0x47 &&
+			Buffer[4]=== 0x0D &&
+			Buffer[5]=== 0x0A &&
+			Buffer[6]=== 0x1A &&
+			Buffer[7]=== 0x0A)
+			return ;
+		else if (mimetype === "image/jpeg" && Buffer.length >= 3 &&
+			Buffer[0]=== 0xFF &&
+			Buffer[1]=== 0xD8 && 
+			Buffer[2]=== 0xFF)
+			return;
+		throw new UnsupportedMediaTypeException('File signature does not match its mimetype !');
 	}
 
 	async udpateData(userId: number, newUsername?: string, newMail?: string, newPassword?: string) {
