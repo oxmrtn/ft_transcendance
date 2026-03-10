33a9605dd32fe450fd751db06e5a6cb00580e8fe
ox_mrtn <martin.trullard@gmail.com>
2026-03-10 22:03:30 +0100

    Add leaderboard endpoint and history endpoint

---
+++ srcs/backend/src/leaderboard/leaderboard.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { LeaderboardService } from './leaderboard.service';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('leaderboard')
+export class LeaderboardController {
+
+	constructor(private leaderboardService: LeaderboardService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+	@Get()
+	async getLeaderboard( @Query('page') page: number = 1) 
+    {
+		return this.leaderboardService.getLeaderboard(Number(page));
+	}
+
+    @UseGuards(AuthGuard('jwt'))
+	@Get('me')
+	async getMyRank(@Req() req : any)
+    {
+		return this.leaderboardService.getMyRank(req.user.userId);
+	}
+    @UseGuards(AuthGuard('jwt'))
+	@Get('around-me')
+	async getAroundMe(@Req() req: any)
+    {
+		return this.leaderboardService.getAroundMe(req.user.userId);
+	}
+}
