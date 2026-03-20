a9a93a46e1e15c179656df76a076dda0c766d44f
Martin Trullard <mtrullar@f4r2s2.clusters.42paris.fr>
2026-03-20 12:23:16 +0100

    fix ubuntu

--- srcs/sandbox/runimage.sh
+++ srcs/sandbox/runimage.sh
@@ -5,7 +5,6 @@ exo_userID=$1
 timeout 10s docker run --rm \
   --network none \
   --memory="128m" \
-  --cpus=".5" \
   --pids-limit 50 \
   -v ./tester/submissions/$exo_userID:/app/$exo_userID.c:ro \
   -v ./tester/results/$exo_userID:/app/results.txt:rw \
